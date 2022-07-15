21a690d256122ad5513fc77dc202c63316e542fe
diogo.lima <diogo@example.com>
2022-07-15 13:42:20 -0300

    fist ci

---
+++ app/app.py
@@ -0,0 +1,15 @@
+import os
+import psycopg
+from flask import Flask, render_template, request
+from prometheus_flask_exporter import PrometheusMetrics
+
+app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+@app.route("/")
+def hello_world():
+
+    return "Ola"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug = False)
