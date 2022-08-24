7185143055932a9b300ec35cd90e4db2bec7e499
diogo.lima <diogo@example.com>
2022-08-24 10:24:02 -0300

    nova versao

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ metrics = PrometheusMetrics(app)
 @app.route("/")
 def hello_world():
 
-    return "nova versao"
+    return "nova versao turmaaaaa"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug = False)
