2f6aba9f36d24739cf473a8de82cfa4ce4f0eca0
Claudinei_Bilcatti <91614332+ClaudineiBilcattiJr@users.noreply.github.com>
2023-05-10 17:51:47 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ metrics = PrometheusMetrics(app)
 @app.route("/")
 def hello_world():
 
-    return "gg du bom!!!!!!123"
+    return "gg du bom!!!!!sdfv!123"
 
 @app.route("/cadastro")
 def cadastro():
